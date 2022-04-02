4419d028554176f9ed591b40394975dabb457977
John Eric <azores.johneric28@gmail.com>
2022-04-02 17:24:37 +0800

    finished working with scanning

--- BACKEND/readingData/main.py
+++ BACKEND/readingData/main.py
@@ -4,8 +4,54 @@ from PyQt5 import  QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget ,QLabel,QLineEdit
 from PyQt5 import QtGui
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
+import sys
 
+import max30102
+import hrcalc
+print("[INFO] MAX30102 Channel & I2C Address.")
+m = max30102.MAX30102()
+hr2 = 0
+sp2 = 0
 
+class Thread(QtCore.QThread):
+    data_sensors = QtCore.pyqtSignal(tuple)
+
+    def run(self):
+        while True:
+            red, ir = m.read_sequential()
+            hr,hrb,sp,spb = hrcalc.calc_hr_and_spo2(ir, red)
+            self.data_sensors.emit((hr,sp,hrb,spb))
 class MainWindow(QDialog):
-     def __init__(self, window):
-         loadUi("windowStart.ui", self)
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("trialUi.ui", self)
+         
+        thread = Thread(self)
+        thread.data_sensors.connect(self.update_Sensors)
+        thread.start()
+        
+    def update_Sensors(self, data):
+        hr, sp , hrb , spb = data
+        hr2 = int(hr)
+        sp2 = int(sp)
+        if(hrb == True and spb ==True):
+            if(hr != -999 and hr<105):
+                self.PulseLbl.setText(str(hr2) + "Bpm")
+                print("heart rate:" + str(hr2))
+            if(sp != -999 and sp < 100):
+                print("Oxygen Saturation :" + str(sp2))
+                self.SPO2Lbl.setText(str(sp2) + "%")
+        else:
+            print("vitals not detected..")
+app = QApplication(sys.argv)
+mainwindow = MainWindow()
+widget = QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedHeight(345)
+widget.setFixedWidth(600)
+widget.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("exiting..")
--- FRONTEND/main.py
+++ FRONTEND/main.py
@@ -8,6 +8,7 @@ from PyQt5 import  QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget ,QLabel,QLineEdit
 from PyQt5 import QtGui
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
 
 #Sensor headers
 import max30102
@@ -16,7 +17,35 @@ from mlx90614 import MLX90614
 import hrcalc
 import time
 
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.IN)
+
+m = max30102.MAX30102()
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
+
+
 #####################################################################################
+#Run Sensors on Thread
+class Thread(QtCore.QThread):
+    data_sensors = QtCore.pyqtSignal(tuple)
+    def run(self):
+        while True:
+           
+            celcius = sensor.get_object_1();
+            faren = (celcius*1.8)+32
+            room = sensor.get_ambient()
+            rt = round(room, 2)       #roomTemp
+            bt = (round(celcius+5,2)) #bodyTemp
+            red, ir = m.read_sequential()
+            hr,hrb,sp,spb = hrcalc.calc_hr_and_spo2(ir, red)
+            
+            
+            
+            self.data_sensors.emit((hr,sp,hrb,spb,rt,bt))
+
 """
 defining a class for the MainWindow
     Content of this class :
@@ -67,10 +96,8 @@ class NewUser(QDialog):
 
     def gotoScanner(self):
         
-        self.thread={} #Initialize a thread
-        #self.thread[1].start()
-        self.scan = Scanner()
-    
+        
+        self.scan = Scanner()     
         widget.addWidget(self.scan)
         widget.setCurrentIndex(widget.currentIndex() + 1)
         newuser = self.txtName.text()
@@ -84,24 +111,46 @@ class Scanner(QDialog):
         loadUi("Scanner.ui", self)
         QtGui.QGuiApplication.processEvents()
         self.btnBack.clicked.connect(self.goBack)
+        thread = Thread(self)
+        thread.data_sensors.connect(self.update_Sensors)
+        thread.start()
         
-       
-        self.InitializeSensor()
-    def InitializeSensor(self):
-        m = max30102.MAX30102()
-        bus = SMBus(1)
-        sensor = MLX90614(bus, address=0x5A)
-        celcius = sensor.get_object_1();
-        faren = (celcius*1.8)+32
-        room = sensor.get_ambient()
-        roomTemp = round(room, 2)
-        bodyTemp = (round(celcius+5,2))
-        hr2 = 0
-        sp2 = 0
+        #set this button to disable when data is not yet scanned
+        self.btnNext.setEnabled(False)
+        self.btnNext_2.setEnabled(False)
+        
+
+    def update_Sensors(self, data):
+        hr, sp , hrb , spb ,rt,bt= data
+        hr2 = int(hr)
+        sp2 = int(sp)
+        
         print("_____________________________________________________")
         print("|\t \t STATUS: SENSORS LOADED    . \t     |")
         print("_____________________________________________________")
-        
+        btnctr = 0
+        if(hrb == True and spb ==True):
+            if(sp != -999 and sp < 100):
+                self.lblBodyTemp.setText(str(bt)+"°")   
+                self.lblOxygenLevel.setText(str(sp2))
+                self.lblRoomTemp.setText(str(rt)+"°")
+                btnctr +=1
+            if(hr != -999 and hr<105):
+                self.lblHeartRate.setText(str(hr2))  # heart rate needs atleast 5-10 seconds and pressure to initialize
+                btnctr += 1
+                ctr = btnctr
+                if (ctr == 2):
+                    self.btnNext.setEnabled(True)
+                    self.btnNext_2.setEnabled(True)
+                    self.btnNext.setStyleSheet("background-color:#FFE2CE;")
+                               
+        else:
+            
+            print("_____________________________________________________")
+            print("|\t \t STATUS: No vitals detected. \t     |")
+            print("_____________________________________________________")
+       
+        """
         while True:
             red, ir = m.read_sequential()
             hr,hrb,sp,spb = hrcalc.calc_hr_and_spo2(ir, red)
@@ -109,20 +158,20 @@ class Scanner(QDialog):
                 hr2 = int(hr)
                 self.lblHeartRate.setText(str(hr2))
                 self.lblBodyTemp.setText(str(bodyTemp)+"°")
-                """print("--------------------")
+                print("--------------------")
                 print("Heart Rate : ",hr2)
-                print("Body Temp  : ",bodyTemp,"\N{DEGREE SIGN}") """
+                print("Body Temp  : ",bodyTemp,"\N{DEGREE SIGN}") 
                 QtGui.QGuiApplication.processEvents()
             if(spb == True and sp != -999):
                 sp2 = int(sp)
                 self.lblOxygenLevel.setText(str(sp2))
                 self.lblRoomTemp.setText(str(roomTemp)+"°")
-                """ print("SPO2       : ",sp2)
+                print("SPO2       : ",sp2)
                 print ("Room Temp  :", roomTemp,u"\N{DEGREE SIGN}C")
-                print("--------------------") """
+                print("--------------------") 
                 time.sleep(12)
                 break
-                QtGui.QGuiApplication.processEvents()
+              
             else:
                 print("_____________________________________________________")
                 print("|\t \t STATUS: No vitals detected. \t     |")
@@ -137,13 +186,32 @@ class Scanner(QDialog):
               )
         return False
         QtGui.QGuiApplication.processEvents()
-        
+        """
     def goBack(self):
         newuser = NewUser()  # <---Instantiate NewUser  Class
         widget.addWidget(newuser)
         widget.setCurrentIndex(widget.currentIndex() -1)  # <----Concat an index number to page 2.
+        self.lblHeartRate.setText("-")
+        self.lblRoomTemp.setText("-")
+        self.lblOxygenLevel.setText("-")
+        self.lblBodyTemp.setText("-")
+        self.btnNext.setEnabled(False)
+        self.btnNext_2.setEnabled(False)
+        self.btnNext.setStyleSheet("background-color:gray")
+        #stop scanner thread after going back
+        thread = Thread(self)
+        thread.data_sensors.connect(self.update_Sensors)
+        thread.quit()
+        print("Thread stopped...")
         
-
+    def sendData(self):
+        self.send = SendSms()     
+        widget.addWidget(self.send)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+class SendSms(QDialog):
+    def __init__(self):
+        super(sms, self).__init__()
+        loadUi("Recepient.ui", self)
 app = QApplication(sys.argv)
 mainwindow = MainWindow()
 widget = QStackedWidget()
